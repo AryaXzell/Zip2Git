7149e41366e24fcf467497547fc4921fdbec67b3
Arya Vallencia <198467490+AryaXzell@users.noreply.github.com>
2026-07-10 00:37:27 +0700

    Upload via Zip2Git 📦

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './contexts/AppContext';
+import { Layout } from './layouts/Layout';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Dashboard } from './pages/Dashboard';
+import { SettingsPage } from './pages/Settings';
+import { ActiveSession } from './pages/ActiveSession';
+import { AboutPage } from './pages/About';
+import { Toaster } from 'react-hot-toast';
+
+export default function App() {
+  return (
+    <AppProvider>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/sesi" element={<ActiveSession />} />
+            <Route path="/tentang" element={<AboutPage />} />
+            <Route path="/pengaturan" element={<SettingsPage />} />
+            {/* Fallback to Home if unknown route */}
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+      <Toaster 
+        position="bottom-right" 
+        toastOptions={{
+          className: 'dark:bg-slate-900 dark:text-white dark:border dark:border-slate-800 text-sm font-sans',
+          duration: 3500,
+        }} 
+      />
+    </AppProvider>
+  );
+}
